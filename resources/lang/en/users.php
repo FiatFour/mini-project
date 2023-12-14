b6dc188588e7596a26ce480e42e12496858b7bc2
sidarat khaobang <sidarat@unixdev.co.th>
2023-12-14 14:46:49 +0700

    commit lang

--- resources/lang/en/users.php
+++ resources/lang/en/users.php
@@ -1,5 +1,9 @@
 <?php
-    return [
-        'page_title' => 'บัญชีผู้ใช้งาน',
-    ];
-?>
+return [
+    'page_title' => 'บัญชีผู้ใช้งาน',
+    'username' => 'ชื่อบัญชีผู้ใช้งาน',
+    'name' => 'ชื่อ-นามสกุล',
+    'email' => 'Email',
+    'password' => 'Password',
+    'phone' => 'เบอร์โทรศัพท์',
+];
